c3e3aff5bbd7479d04d38191d2b613501246d145
lo <work>
2026-04-13 16:27:03 +0800

    s

--- proj/src/pages/Home.tsx
+++ proj/src/pages/Home.tsx
@@ -4,15 +4,19 @@ import { router } from '../main'
 import { listPageNames } from '../main'
 
 const Home: React.FC = () => {
-  const pages = [['KANA', ''], ...listPageNames, ['PARSER-TOOL', ''], ['LIST-TOOL', '']]
+  const pages = [['KANA', ''], ...listPageNames, ['PARSER-TOOL', ''], ['LIST-TOOL', ''], ['👊 word-lists', '/word-lists/index.html']]
   return (
     <div className={Styles.intro}>
       {
         pages.map((it) => (
           <div className={Styles.page}
             onClick={() => {
-              router.navigate(`/${it[0].toLowerCase().split(' ')[0]}`)
-          }}>{it[0]}</div>
+              if (it[1]) {
+                window.open(it[1])
+              } else {
+                router.navigate(`/${it[0].toLowerCase().split(' ')[0]}`)
+              }
+            }}>{it[0]}</div>
         ))
       }
     </div>
